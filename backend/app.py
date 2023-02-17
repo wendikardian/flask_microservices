f7e8d9cc77618f08a1712401dd90af7623ff2ab4
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-02-17 23:46:20 +0700

    backend finished

--- backend/app.py
+++ backend/app.py
@@ -2,19 +2,15 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from flask_marshmallow import Marshmallow
+
+
 app = Flask(__name__)
 
 # Decorators in Python
 
-
 # To activate debug mode set FLASK_DEBUG=1
 
 
-# @app.route('/')
-# @app.route('/home')
-
-# def hello_world():
-#     # return '<h1>hello, World ! :) 123 123</h1>'
 #     return render_template('home.html')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
@@ -22,7 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-app.app_context().push()
+# app.app_context().push()
 
 class Articles(db.Model):
     id = db.Column(db.Integer, primary_key  = True)
@@ -38,22 +34,56 @@ class Articles(db.Model):
 
 class ArticleSchema(ma.Schema):
     class Meta:
-        field = ('id', 'title', 'body', 'date')
+        fields = ('id', 'title', 'body', 'date')
 
 article_schema =  ArticleSchema()
 articles_schema =  ArticleSchema(many = True)
 
-
+# @app.route('/')
+# # @app.route('/home')
+# def hello_world():
+#     return '<h1>hello, World ! :) 123 123</h1>'
 
 @app.route('/get', methods = ['GET'])
 def get_articles():
-    return jsonify({"Hello" : "World"})
+    # return jsonify({"Hello" : "World"})
+    all_articles = Articles.query.all()
+    # results = [{'id': row.id, 'title': row.title, 'body': row.body, 'date' : row.date} for row in all_articles]
+    results = articles_schema.dump(all_articles)
+    return jsonify(results)
+
+@app.route('/get/<id>/', methods = ['GET'])
+def post_details(id):
+    article = Articles.query.get(id)
+    return article_schema.jsonify(article)
+
 
 @app.route('/add', methods = ['POST'])
 def add_article():
     title = request.json['title']
     body = request.json['body']
+    articles = Articles(title, body)
+    db.session.add(articles)
+    db.session.commit()
+    return article_schema.jsonify(articles)
 
+@app.route('/update/<id>/', methods = ['PUT'])
+def update_article(id):
+    article = Articles.query.get(id)
+    title = request.json['title']
+    body = request.json['body']
+    article.title = title
+    article.body = body
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+@app.route('/delete/<id>/', methods = ['DELETE'])
+def article_delete(id):
+    article = Articles.query.get(id)
+    db.session.delete(article)
+    db.session.commit()
+
+    return article_schema.jsonify(article)
 
 if __name__ == "__main__":
     app.run(debug=True)
